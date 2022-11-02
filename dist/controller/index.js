d5304dfebb852d6136a53fcb5d12bc357b988e31
syedharis786 <syedharis436@gmail.com>
2022-11-02 16:29:30 +0500

    First Commit

---
+++ dist/controller/index.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.baseRoutController = void 0;
+const baseRoutController = (req, res) => {
+    res.json({
+        message: 'Hello World!'
+    });
+};
+exports.baseRoutController = baseRoutController;
+//# sourceMappingURL=index.js.map
---
+++ src/controller/index.ts
@@ -0,0 +1,7 @@
+
+
+export const baseRoutController = (req, res) => {
+    res.json({
+      message: 'Hello World!'
+    })
+  }
