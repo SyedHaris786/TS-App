68f0128f24a0cf38b8d832f6e088cd7ee69bcf06
syedharis786 <syedharis436@gmail.com>
2022-11-30 12:52:57 +0500

    Added email check in registering account

--- src/controller/register.ts
+++ src/controller/register.ts
@@ -1,5 +1,6 @@
 const bcrypt = require('bcrypt');
 import { register } from "../repo/register";
+import { creds } from "../repo/login";
 
 export const auth = async (req: any, res: any) => {
 
@@ -14,10 +15,15 @@ export const auth = async (req: any, res: any) => {
 
   try {
 
+    const getCreds = await creds(email);
     if (!username || !email || !password || !phone_number) {
       res.json("Please enter all values");
 
-    } else {
+    } else if (getCreds[0].email || getCreds.length > 0) {
+
+      res.json("Email already exist")
+    }
+    else {
 
       let hashedPassword = await bcrypt.hash(password, 5);
       password = hashedPassword;
