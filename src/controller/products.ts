4431edcc9835292ed85ebccb9aab52bc6467db70
syedharis786 <syedharis436@gmail.com>
2022-12-13 12:29:47 +0500

    added admin check on add product and added update product

--- src/controller/products.ts
+++ src/controller/products.ts
@@ -1,6 +1,7 @@
 import { Products } from "../repo/entities/Products"
 import { DataSource } from "typeorm"
 import { AppDataSource } from "../repo/connectdb"
+import { Users } from "../repo/entities/Users"
 
 //To see single product details
 export const product = async (req: any, res: any) => {
@@ -30,7 +31,9 @@ export const allProducts = async (req: any, res: any) => {
 
 export const addProduct = async (req: any, res: any) => {
 
-    const { productName,
+    const {
+        userId,
+        productName,
         category,
         price,
         productDescription,
@@ -41,15 +44,36 @@ export const addProduct = async (req: any, res: any) => {
     } else {
 
         try {
-            const insertProduct = await AppDataSource
-                .getRepository(Products)
-                .createQueryBuilder("products")
-                .insert()
-                .into(Products)
-                .values({ productName, category, price, productDescription, stockQty: qty })
-                .execute();
 
-            res.json(insertProduct)
+            const validateAdmin = await AppDataSource
+                .getRepository(Users)
+                .createQueryBuilder("users")
+                .select(`"isAdmin"`)
+                .where(`user_id = ${userId}`)
+                .getRawMany();
+
+
+            const adminCheck = validateAdmin[0].isAdmin
+
+            if (adminCheck === true) {
+
+                const insertProduct = await AppDataSource
+                    .getRepository(Products)
+                    .createQueryBuilder("products")
+                    .insert()
+                    .into(Products)
+                    .values({ productName, category, price, productDescription, stockQty: qty })
+                    .execute();
+
+                res.json(`Added product ${productName} Successfully! `);
+                console.log(`Added product ${productName} Successfully! `);
+
+
+            } else {
+                console.log(`User Id: ${userId} tried to enter product`);
+                res.json(`You don't have privilege to add product`)
+
+            }
         } catch (err) {
 
 
@@ -57,5 +81,42 @@ export const addProduct = async (req: any, res: any) => {
             console.log(err);
 
         }
+    }
+}
+
+
+export const updateProduct = async (req: any, res: any) => {
+
+    const {
+        productId,
+        category,
+        price,
+        productDescription,
+        qty } = req.body;
+
+
+    const updatedItems = {
+        category: category,
+        price: price,
+        productDescription: productDescription,
+        stockQty: qty
+    }
+
+    try {
+
+
+        const updateProduct = await AppDataSource.
+            getRepository(Products)
+            .createQueryBuilder()
+            .update(Products)
+            .set(updatedItems)
+            .where(`product_id = ${productId}`)
+            .execute()
+
+        res.json(updateProduct);
+
+    } catch (err) {
+        console.log(err);
+
     }
 }
--- src/route/products.ts
+++ src/route/products.ts
@@ -1,15 +1,20 @@
 import * as express from 'express'
 import { auth } from '../middleware/authValidation';
-import { product, allProducts, addProduct } from '../controller/products'
+import { product, allProducts, addProduct, updateProduct } from '../controller/products'
 
 const router = express.Router();
 
 
 
-// Select Routes
+// Get products Routes
 router.route('/product/:id').get(auth, product)
 router.route('/products').get(auth, allProducts)
 
+
+//For Adding a product
 router.route('/product/').post(addProduct)
+
+//Updating a Product
+router.route('/product/update').post(updateProduct)
 
 module.exports = router
